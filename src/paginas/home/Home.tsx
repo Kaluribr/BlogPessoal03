923c0a5d3991420e18590722760a83bd48cab0e4
Danilo Moraes <danilot.moraes@gmail.com>
2023-04-13 00:16:40 -0300

    Tentatica 1 de footer

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,32 @@
-import './Home.css';
+import "./Home.css";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-
-function Home(){
-    return(
-       <>
-        <h1>Home</h1>
-       </>
-    )
+function Home() {
+  return (
+    <>
+      <Box sx={{ flexGrow: 6}}>
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={6}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 }
 
 export default Home;
